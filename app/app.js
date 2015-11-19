ddd21617c81725c742e394cd36fc011f10277f75
AA <andrewhkg@hotmail.com>
2015-11-19 18:11:40 +0800

    Installed WebSockets for twitter app

--- app/app.js
+++ app/app.js
@@ -1,8 +1,12 @@
+require('dotenv').load();
+
 var morgan  = require('morgan');
 var express = require('express');
 var app     = express();
+var server  = require('http').createServer(app);
 var port    = process.env.PORT || 3000;
 var router  = express.Router();
+var Twit = require('twit');
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -31,6 +35,33 @@ router.get('/about', function(req, res) {
 });
 
 app.use('/', router);
-app.listen(port);
+server.listen(port);
 
 console.log('Server started on ' + port);
+console.log(process.env.TWITTER_CONSUMER_KEY);
+var io = require('socket.io')(server);
+
+var twitter = new Twit({
+  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+  access_token:         process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var stream = twitter.stream('statuses/filter', { track: 'javascript' });
+
+console.log(twitter);
+
+io.on('connect', function(socket) {
+  stream.on('tweet', function (tweet) {
+    var data = {
+      name: tweet.user.name,
+      screen_name: tweet.user.screen_name,
+      text: tweet.text,
+      user_profile_image:  tweet.user.profile_image_url
+    };
+      socket.emit('tweets', data);
+    });
+});
+
+
